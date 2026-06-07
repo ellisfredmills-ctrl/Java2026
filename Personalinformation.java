5bfbda2deaa12486519c64da943806b3536d45fb
Fredrick Ellis Mills <ellisfredmills@gmail.com>
2026-06-07 17:11:45 +0000

    Add files via upload

---
+++ Personalinformation.java
@@ -0,0 +1,33 @@
+ public class Personalinformation{
+   
+   public static void main(String[] agrs){
+
+     String fullname   = "Fredrick Mills";
+     int     age       = 21;
+     char    gender    = 'M';
+     double  height    = 1.75;
+     float   weight    = 68.5f;
+     boolean isStudent = true;
+     long phoneNumber  = 233539477076L;
+     String grade      = "Level 200";
+     String major      = "Computer Networking";
+     double gpa        = 3.75;
+
+     
+     System.out.println("Full Name    : " + fullname);
+     System.out.println("Age          : " + age + " years");
+     System.out.println("Gender       : " + gender);
+     System.out.println("Height       : " + height + " m");
+     System.out.println("Weight       : " + weight + " kg");
+     System.out.println("Students     : " + isStudent);
+     System.out.println("Phone Number : " + phoneNumber);
+     System.out.println("Grade/Level  : " + grade);
+     System.out.println("Major        : " + major);
+     System.out.println("GPA          : " + gpa);
+   
+
+
+  }
+
+
+}
