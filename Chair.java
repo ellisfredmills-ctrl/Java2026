a773b08993fc1edc2a674e8cdc4e17d073084200
Fredrick Ellis Mills <ellisfredmills@gmail.com>
2026-06-09 13:22:23 +0000

    Add files via upload

---
+++ Chair.java
@@ -0,0 +1,20 @@
+public class Chair {
+    public static void main(String[] args) {
+
+
+        System.out.println("X                                                                               X ");
+        System.out.println("X                                                                               X ");
+        System.out.println("X                                                                               X ");
+        System.out.println("X                                                                               X ");
+        System.out.println("X                                                                               X ");
+        System.out.println("X                                                                               X ");
+        System.out.println("X X X X X X X X X X X X        X X X X X X X X X X X X X    X X X X X X X X X X X ");
+        System.out.println("X                     X        X                       X    X                   X ");
+        System.out.println("X                     X        X                       X    X                   X ");
+        System.out.println("X                     X        X                       X    X                   X ");
+        System.out.println("X                     X        X                       X    X                   X ");
+        System.out.println("X                     X        X                       X    X                   X ");
+        System.out.println("X                     X        X                       X    X                   X ");
+        System.out.println("X                     X        X                       X    X                   X ");
+    }
+}
---
+++ Details.java
@@ -0,0 +1,18 @@
+public class Details {
+    public static void main(String[] args) {
+        String name = " Fredrick Mills";
+        int age =  21;
+        char sex = 'M';
+        long phone = 233539477076L;
+        boolean student = true;
+        float height = 175.344F;
+
+
+        System.out.println("My names is " +name);
+        System.out.println("My age is " +age);
+        System.out.println("Male or Female " +sex);
+        System.out.println("My phone number is " +phone);
+        System.out.println("I'm a studnet " +student);
+        System.out.println("My height is " +height);
+    }
+}
